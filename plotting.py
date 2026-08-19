83507956cd7d2c8d05f22cb0b3f858d73c59cc78
Mario Boley <mario.boley@gmail.com>
2026-08-19 08:25:19 +0300

    refactoring

--- plotting.py
+++ plotting.py
@@ -59,9 +59,20 @@ def apply(method, objs, *args, **kwargs):
         a = np.asarray(a)
         if a.shape[:objs.ndim] == objs.shape:
             return (a[idx] for idx in np.ndindex(objs.shape))
-        return repeat(a)
+        return repeat(a.item() if a.ndim == 0 else a)
+
+    kw_iters = {k: _iter(v) for k, v in kwargs.items()}
     for vals in zip(objs.flat, *[_iter(a) for a in args]):
-        method(*vals, **kwargs)
+        method(*vals, **{k: next(it) for k, it in kw_iters.items()})
+
+    # from itertools import repeat
+    # def _iter(a):
+    #     a = np.asarray(a)
+    #     if a.shape[:objs.ndim] == objs.shape:
+    #         return (a[idx] for idx in np.ndindex(objs.shape))
+    #     return repeat(a)
+    # for vals in zip(objs.flat, *[_iter(a) for a in args]):
+    #     method(*vals, **kwargs)
 
 def centre_spines(ax: Axes):
     for spine in ['left', 'bottom']:
@@ -179,168 +190,37 @@ def superimpose_axis(ax, positions, labels=None, **kwargs):
 
 Axes.superimpose_axis = superimpose_axis
 
+def histo(ax, y, bins=None, range=None):
+    """Plots histogram density with correct normalisation by overall
+    number of data points (instead of only number of datapoints falling into
+    range as in matplotlib.pyplot.histo)
+    """
+    bins = bins if bins is not None else int((np.log2(len(y))))
+    counts, edges = np.histogram(y, bins=bins, range=range)
+    ax.stairs(counts / (len(y) * (edges[1] - edges[0])), edges, fill=True)
+
 def plot_pmf(ax, x, pmf):
     p = pmf(x)
     ax.vlines(x, 0, p, colors='k', linestyles='--')
     ax.scatter(x, p, facecolors='white', linewidths=1.5, edgecolors='k', zorder=3)
 
-def histo_1d(ax, y, pmf=None):
-    x = np.arange(y.min(), y.max()+1)
-    ax.hist(y, np.arange(y.min() - 0.5, y.max() + 1.5), density=True, align='mid')
-    if pmf is not None: plot_pmf(ax, x, pmf)
-        # p = pmf(x)
-        # ax.vlines(x, 0, p, colors='k', linestyles='--')
-        # ax.scatter(x, p, facecolors='white', linewidths=1.5, edgecolors='k', zorder=3)
-    ax.set_ylim(ax.get_ylim()*np.array([0, 1.01]))
-    ax.margins(x=0)
-    ax.set_xlabel('$x$')
-    ax.set_ylabel(r'$|\{i: x_i = x\}|/n$')
+def histo_int(ax, y, a=None, b=None, width=0.6, pmf=None):
+    """Plots a histogram for integer data with bins that are centred around the 
+    corresponding integer value and an optional pmf overlay.
+    """
+    y = np.asarray(y)
+    a = a if a is not None else y.min()
+    b = b if b is not None else y.max()
+    counts, _ = np.histogram(y, bins=np.arange(a - 0.5, b + 1.5))
+    ax.bar(np.arange(a, b + 1), counts / len(y), width=width)
+    if pmf is not None: plot_pmf(ax, np.arange(a, b + 1), pmf)
 
-# def superimpose_axis(ax, positions, labels=None, axis='x', side='positive',
-#                      color='k', tick_size=5, label_pad=4, fontsize=9, rotation=0,
-#                      limits=None):
-#     labels = labels or [f'{p:g}' for p in positions]
-#     phi = rotation
-#     if axis == 'x':
-#         ax_dir   = np.array([ np.cos(phi),  np.sin(phi)])
-#         tick_dir = np.array([-np.sin(phi),  np.cos(phi)])
-#     else:
-#         ax_dir   = np.array([-np.sin(phi),  np.cos(phi)])
-#         tick_dir = np.array([ np.cos(phi),  np.sin(phi)])
+# def histo_1d(ax, y, pmf=None):
+#     x = np.arange(y.min(), y.max()+1)
+#     ax.hist(y, np.arange(y.min() - 0.5, y.max() + 1.5), density=True, align='mid')
+#     if pmf is not None: plot_pmf(ax, x, pmf)
+#     ax.set_ylim(ax.get_ylim()*np.array([0, 1.01]))
+#     ax.margins(x=0)
+#     ax.set_xlabel('$x$')
+#     ax.set_ylabel(r'$|\{i: x_i = x\}|/n$')
 
-#     sign = 1 if side == 'positive' else -1
-#     tick_dir = sign * tick_dir
-
-#     inv      = ax.transData.inverted()
-#     origin_d = ax.transData.transform([0.0, 0.0])
-#     tick_d   = ax.transData.transform(tick_dir) - origin_d
-#     tick_d  /= np.linalg.norm(tick_d)
-#     tick_vec = inv.transform(origin_d + tick_d) - inv.transform(origin_d)
-
-#     ha = 'left'   if tick_vec[0] > 0 else ('right'  if tick_vec[0] < 0 else 'center')
-#     va = 'bottom' if tick_vec[1] > 0 else ('top'    if tick_vec[1] < 0 else 'center')
-
-#     if limits is not None:
-#         p1, p2 = limits[0] * ax_dir, limits[1] * ax_dir
-#         ann = ax.annotate('', xy=p2, xytext=p1,
-#                           arrowprops=dict(arrowstyle='->', color=color, lw=0.8,
-#                                           shrinkA=0, shrinkB=0),
-#                           annotation_clip=False)
-#         ann.arrow_patch.set_clip_on(False)
-
-#     for pos, lbl in zip(positions, labels):
-#         c  = pos * ax_dir
-#         t  = c + tick_size * tick_vec
-#         ax.plot([c[0], t[0]], [c[1], t[1]], color=color, lw=0.8, clip_on=False)
-#         lp = c + (tick_size + label_pad) * tick_vec
-#         ax.text(lp[0], lp[1], lbl, ha=ha, va=va, color=color, fontsize=fontsize)
-
-
-# def superimpose_axis(ax, positions, labels=None, axis='x', side='positive',
-#                      color='k', tick_size=5, label_pad=4, fontsize=9, rotation=0,
-#                      limits=None):
-#     labels = labels or [f'{p:g}' for p in positions]
-#     phi = rotation
-#     if axis == 'x':
-#         ax_dir   = np.array([ np.cos(phi),  np.sin(phi)])
-#         tick_dir = np.array([-np.sin(phi),  np.cos(phi)])
-#     else:
-#         ax_dir   = np.array([-np.sin(phi),  np.cos(phi)])
-#         tick_dir = np.array([ np.cos(phi),  np.sin(phi)])
-
-#     sign = 1 if side == 'positive' else -1
-#     tick_dir = sign * tick_dir
-
-#     # convert tick_dir to a vector of length tick_size in pixels
-#     inv      = ax.transData.inverted()
-#     origin_d = ax.transData.transform([0.0, 0.0])
-#     tick_d   = ax.transData.transform(tick_dir) - origin_d
-#     tick_d  /= np.linalg.norm(tick_d)                              # 1 px unit vector
-#     tick_vec = inv.transform(origin_d + tick_d) - inv.transform(origin_d)  # in data coords
-
-#     ha = 'left'   if tick_d[0] > 0 else ('right'  if tick_d[0] < 0 else 'center')
-#     va = 'bottom' if tick_d[1] < 0 else ('top'    if tick_d[1] > 0 else 'center')
-
-#     if limits is not None:
-#         p1, p2 = limits[0] * ax_dir, limits[1] * ax_dir
-#         ann = ax.annotate('', xy=p2, xytext=p1,
-#                           arrowprops=dict(arrowstyle='->', color=color, lw=0.8,
-#                                           shrinkA=0, shrinkB=0),
-#                           annotation_clip=False)
-#         ann.arrow_patch.set_clip_on(False)
-
-#     for pos, lbl in zip(positions, labels):
-#         c = pos * ax_dir
-#         t = c + tick_size * tick_vec
-#         ax.plot([c[0], t[0]], [c[1], t[1]], color=color, lw=0.8, clip_on=False)
-#         lp = c + (tick_size + label_pad) * tick_vec
-#         ax.text(lp[0], lp[1], lbl, ha=ha, va=va, color=color, fontsize=fontsize)
-
-
-# def superimpose_axis(ax, positions, labels=None, axis='x', side='positive',
-#                      color='k', tick_size=5, label_pad=4, fontsize=9, rotation=0,
-#                      limits=None):
-#     labels = labels or [f'{p:g}' for p in positions]
-#     phi = rotation
-#     if axis == 'x':
-#         ax_dir = np.array([ np.cos(phi),  np.sin(phi)])
-#     else:
-#         ax_dir = np.array([-np.sin(phi),  np.cos(phi)])
-
-#     # perpendicular in display space (pixels)
-#     inv = ax.transData.inverted()
-#     origin_d = ax.transData.transform([0.0, 0.0])
-#     ax_dir_d = ax.transData.transform(ax_dir) - origin_d
-#     ax_dir_d /= np.linalg.norm(ax_dir_d)
-#     sign = 1 if side == 'positive' else -1
-#     perp_d = sign * np.array([-ax_dir_d[1], ax_dir_d[0]])  # screen-space perpendicular
-
-#     if limits is not None:
-#         p1, p2 = limits[0] * ax_dir, limits[1] * ax_dir
-#         ann = ax.annotate('', xy=p2, xytext=p1,
-#                           arrowprops=dict(arrowstyle='->', color=color, lw=0.8,
-#                                           shrinkA=0, shrinkB=0),
-#                           annotation_clip=False)
-#         ann.arrow_patch.set_clip_on(False)
-
-#     for pos, lbl in zip(positions, labels):
-#         c   = pos * ax_dir
-#         c_d = ax.transData.transform(c)
-#         t   = inv.transform(c_d + tick_size * perp_d)
-#         ax.plot([c[0], t[0]], [c[1], t[1]], color=color, lw=0.8, clip_on=False)
-#         lp  = inv.transform(c_d + (tick_size + label_pad) * perp_d)
-#         ha  = 'left'   if perp_d[0] > 0 else ('right'  if perp_d[0] < 0 else 'center')
-#         va  = 'bottom' if perp_d[1] > 0 else ('top'    if perp_d[1] < 0 else 'center')
-#         ax.text(lp[0], lp[1], lbl, ha=ha, va=va, color=color, fontsize=fontsize)
-
-
-# def superimpose_axis(ax: Axes, positions, labels=None, axis='x', side='positive',
-#                      color='k', tick_size=0.15, fontsize=9, rotation=0.0,
-#                      limits=None):
-#     labels = labels or [f'{p:g}' for p in positions]
-#     phi = rotation
-#     if axis == 'x':
-#         ax_dir = np.array([ np.cos(phi),  np.sin(phi)])
-#         perp   = np.array([-np.sin(phi),  np.cos(phi)])
-#     else:
-#         ax_dir = np.array([-np.sin(phi),  np.cos(phi)])
-#         perp   = np.array([ np.cos(phi),  np.sin(phi)])
-
-#     sign = 1 if side == 'positive' else -1
-#     dp = sign * perp
-
-#     if limits is not None:
-#         p1, p2 = limits[0] * ax_dir, limits[1] * ax_dir
-#         ann = ax.annotate('', xy=p2, xytext=p1,
-#                   arrowprops=dict(arrowstyle='<->', color=color, lw=0.8, shrinkA=0, shrinkB=0),
-#                   annotation_clip=False)
-#         ann.arrow_patch.set_clip_on(False)
-
-#     for pos, lbl in zip(positions, labels):
-#         c  = pos * ax_dir
-#         t  = c + tick_size * dp
-#         ax.plot([c[0], t[0]], [c[1], t[1]], color=color, lw=0.8, clip_on=False)
-#         lp = c + 1.8 * tick_size * dp
-#         ha = 'left' if dp[0] > 0 else ('right' if dp[0] < 0 else 'center')
-#         va = 'bottom' if dp[1] > 0 else ('top'  if dp[1] < 0 else 'center')
-#         ax.text(lp[0], lp[1], lbl, ha=ha, va=va, color=color, fontsize=fontsize)
